d67a7b65f68adccdddd274ab9d9dda554532cb1c
Matis Rammi <matis.rammi@khk.ee>
2022-01-24 16:44:29 +0200

    Update files

--- myfirst.js
+++ myfirst.js
@@ -1,20 +1,12 @@
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Updated!');
 });
 
-var fs = require('fs');
-
-fs.open('mynewfile2.txt', 'w', function (err, file) {
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
     if (err) throw err;
-    console.log('Saved!');
+    console.log('Replaced!');
 });
 
-var fs = require('fs');
-
-fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-});
